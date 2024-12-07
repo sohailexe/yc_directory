3fd317b5c156b37ebaa38e187a169eca7bfcc4ac
sohailexe <div.sohailfaiz@gmail.com>
2024-12-07 10:50:53 +0500

    Startup card Implemented

--- app/(ROOT)/page.js
+++ app/(ROOT)/page.js
@@ -1,7 +1,37 @@
 import SearchForm from "@/app/components/SearchForm";
+import StartUpCard from "@/app/components/StartUpCard";
 
 export default async function Home({ searchParams }) {
   const { query } = await searchParams;
+
+  let posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Sohail",
+      },
+      _id: 1,
+      description: "This is image",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN9NShuPDUQANXTUDJ90ikqaN-1x_YtbKcJQ&s",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
+  posts = posts.filter((post) => {
+    if (!query) return post;
+
+    const search = query.toLowerCase();
+    return (
+      post.title.toLowerCase().includes(search) ||
+      post.description.toLowerCase().includes(search) ||
+      post.category.toLowerCase().includes(search)
+    );
+  });
+
   return (
     <>
       <section className="pink_container">
@@ -15,6 +45,21 @@ export default async function Home({ searchParams }) {
           dolor sit.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {" "}
+          {query ? `Search results for " ${query}"` : "All posts"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartUpCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">NO Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
